969d66a4f9b161b1e12dd51d5852b99715bd521e
robbbowes <up666724@myport.ac.uk>
2023-05-02 15:00:29 +0200

    Fix nullable issue

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
 
 namespace dotnet7_rpg.Services.CharacterService
 {
@@ -143,7 +139,7 @@ namespace dotnet7_rpg.Services.CharacterService
                     return response;
                 }
 
-                character.Skills.Add(skill);
+                character.Skills!.Add(skill);
                 await _context.SaveChangesAsync();
 
                 response.Data = _mapper.Map<GetCharacterResponseDto>(character);
